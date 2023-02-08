e6b750581ad0c6a1a33540e79918bcb25ac785f8
Veronchi <zhigunova1995@gmail.com>
2023-02-09 01:15:16 +0300

    feat: add routes

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import sequelize from "./db";
 import path from "node:path";
 import errorHadler from './middleware/ErrorHandlingMiddleware';
 import userRoute from "./resource/user/user.routes";
+import boardRoute from "./resource/board/board.routes";
+import columnRoute from "./resource/column/column.routes";
+import rowRoute from "./resource/row/row.routes";
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -14,6 +17,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "static")));
 app.use('/api/user', userRoute);
+app.use('/api/board', boardRoute);
+app.use('/api/column', columnRoute);
+app.use('/api/row', rowRoute);
 
 app.use(errorHadler);
 
