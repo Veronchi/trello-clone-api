fa14cd1aec3942b0cf325abe16ffacb8d70d4407
Veronchi <zhigunova1995@gmail.com>
2023-02-09 01:11:52 +0300

    feat: add errors

--- src/error/ApiError.ts
+++ src/error/ApiError.ts
@@ -9,14 +9,22 @@ export default class ApiError extends Error {
   }
 
   static badRequest(message: string): ApiError {
+    return new ApiError(400, message)
+  }
+  
+  static unauthorized(message: string): ApiError {
+    return new ApiError(401, message);
+  }
+  
+  static forbidden(message: string): ApiError {
+    return new ApiError(403, message);
+  }
+
+  static notFound(message: string): ApiError {
     return new ApiError(404, message)
   }
 
   static internal(message: string): ApiError {
     return new ApiError(500, message);
-  }
-
-  static forbidden(message: string): ApiError {
-    return new ApiError(403, message);
   }
 }
